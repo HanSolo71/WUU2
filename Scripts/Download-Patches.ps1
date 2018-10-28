4a3c9ee03ecf628206b8c7114956893ecbb9c092
HanSolo71 <HanSolo552@gmail.com>
2018-10-28 16:50:49 -0400

    Add files via upload

---
+++ Scripts/Download-Patches.ps1
@@ -0,0 +1,22 @@
+﻿$updateSession = New-Object -ComObject 'Microsoft.Update.Session'
+$updateSearcher = $updateSession.CreateupdateSearcher()
+$searchResult = $updateSearcher.Search("IsInstalled=0 and IsHidden=0")
+$updatesToDownload = New-Object -ComObject "Microsoft.Update.UpdateColl"
+ForEach($update in $searchResult.Updates){
+    if($update.IsDownloaded -eq $false){
+        $updatesToDownload.Add($update) | Out-Null
+    }
+}
+if($updatesToDownload.Count -gt 0){
+    $downloader = $updateSession.CreateUpdateDownloader()
+    $downloader.Updates = $updatesToDownload
+    $downloadResult = $downloader.Download()
+
+    $numDownloaded = 0
+    0..($updatesToDownload.Count - 1) | % {
+        $result = $downloadResult.GetUpdateResult($_).ResultCode
+        if($result -eq 2 -or $result -eq 3){$numDownloaded++}
+    }
+    return $numDownloaded
+}
+else{return 0}
