bacb0113b72b8056cfaea4461267c7bac170659c
HanSolo71 <HanSolo552@gmail.com>
2018-10-28 16:50:33 -0400

    Add files via upload

---
+++ Scripts/Install-Patches.ps1
@@ -0,0 +1,24 @@
+﻿$updateSession = New-Object -ComObject 'Microsoft.Update.Session'
+$updateSearcher = $updateSession.CreateupdateSearcher()
+$searchResult = $updateSearcher.Search("IsInstalled=0 and IsHidden=0")
+
+if($searchResult.updates.count -eq 0){return 0}
+
+$updatesToInstall = New-Object -ComObject "Microsoft.Update.UpdateColl"
+
+ForEach($update in $searchResult.Updates){
+    if($update.InstallationBehavior.CanRequestUserInput -eq $true){continue}
+    if($update.IsDownloaded -eq $false){continue}
+    if($update.EulaAccepted -eq $false){$update.AcceptEula()}
+    $updatesToInstall.Add($update) | Out-Null
+}
+
+$installer = $updateSession.CreateUpdateInstaller()
+$installer.Updates = $updatesToInstall
+$installationResult = $installer.Install()
+
+$numErrors = 0
+0..($updatesToInstall.Count - 1) | % {
+    if($installationResult.GetUpdateResult($_).ResultCode -ge 4){$numErrors++}
+}
+return $numErrors
